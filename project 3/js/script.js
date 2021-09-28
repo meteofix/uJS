bcc27bf283980a00c029631d84fdb25744df8ee4
meteofix <meteofix@gmail.com>
2021-09-28 22:20:38 +0300

    Practice 3.3 (modal)

--- project 3/js/script.js
+++ project 3/js/script.js
@@ -70,6 +70,41 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
+    // === === === timer end === === ===
 
+    // === === === modal start === === ===
+    const modal = document.querySelector('.modal'),
+        modalCloseButtons = modal.querySelectorAll('[data-close]'),
+        modalOpenButtons = document.querySelectorAll('[data-modal]'),
+        offer2 = document.querySelector('.offer__action')
+
+    function modalOpen () {
+        modal.classList.add('show')
+        modal.classList.remove('hide')
+        document.body.style.overflow = 'hidden'
+    }
+    function modalClose () {
+        modal.classList.add('hide')
+        modal.classList.remove('show')
+        document.body.style.overflow = ''
+    }
+
+    modalOpenButtons.forEach((item) => {
+        item.addEventListener('click', modalOpen)
+    })
+    modalCloseButtons.forEach((item) => {
+        item.addEventListener('click', modalClose)
+    })
+
+    modal.addEventListener('click', (e) => {
+        if ( e.target === modal) {
+            modalClose();
+        }
+    })
+    document.addEventListener('keydown',  (e) => {
+        if (e.code === "Escape" && modal.classList.contains('show')) {
+            modalClose();
+        }
+    })
 
 });
